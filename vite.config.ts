6cc58cc313a566a116b46465c63afc2c3a5472ef
aleksey <av.pavlow@gmail.com>
2026-09-07 10:19:02 +0300

    add: Добавляем к проекту tailwind и его настройки

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,18 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    tailwindcss(), // Регистрируем плагин Tailwind v4
   ],
   resolve: {
     alias: {
-     '@app': fileURLToPath(new URL('./src/app', import.meta.url)),
+      '@app': fileURLToPath(new URL('./src/app', import.meta.url)),
       '@pages': fileURLToPath(new URL('./src/pages', import.meta.url)),
       '@widgets': fileURLToPath(new URL('./src/widgets', import.meta.url)),
       '@features': fileURLToPath(new URL('./src/features', import.meta.url)),
